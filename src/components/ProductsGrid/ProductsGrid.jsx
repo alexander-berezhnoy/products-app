5476b29a76b37dc4e5e3e7570e479a56a48b0f3e
alexander-berezhnoy <berrezhnoy@gmail.com>
2020-07-03 17:37:14 +0300

    Update project structure

--- src/components/ProductsGrid/ProductsGrid.jsx
+++ src/components/ProductsGrid/ProductsGrid.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import { 
     Grid 
 } from '@material-ui/core';
-import ProductCard from './ProductCard'
+import ProductCard from '../ProductCard'
 
 const useStyles = makeStyles({
     container: {
