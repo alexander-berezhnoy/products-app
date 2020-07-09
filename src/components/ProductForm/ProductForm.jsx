2e3590f642aa1c314268f91faa8649bead0680c3
Oleksandr Berezhnyi <berrezhnoy@gmail.com>
2020-07-09 11:14:50 +0300

    Update product form with Formik

--- src/components/ProductForm/ProductForm.jsx
+++ src/components/ProductForm/ProductForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useFormik } from 'formik';
+import { Formik, Form, useField } from 'formik';
 import './style.scss';
 
 const validate = values => {
@@ -24,43 +24,85 @@ const validate = values => {
   return errors;
 };
 
-const ProductForm = ({ product, handleRequest }) => {
-  const formik = useFormik({
-    initialValues: {
-      name: product.name,
-      picture: product.picture,
-      description: product.description,
-      price: product.price
-    },
-    validate,
-    onSubmit: values => { // handleRequest
-      alert(JSON.stringify(values, null, 2));
-      handleRequest();
-    }
-  });
-
-  return(
-    <form onSubmit={formik.handleSubmit} className="form">
-      <label htmlFor="name" className="label">Name</label>
-      <input name="name" className="field" {...formik.getFieldProps('name')} />
-      { formik.touched.name && formik.errors.name ? <div className="error">{formik.errors.name}</div> : null}
-
-      <label htmlFor="picture" className="label">Picture</label>
-      <input name="picture" className="field" {...formik.getFieldProps('picture')} />
-      { formik.touched.picture && formik.errors.picture ? <div className="error">{formik.errors.picture}</div> : null}
-
-      <label htmlFor="description" className="label">Description</label>
-      <textarea name="description" className="field" {...formik.getFieldProps('description')} rows="5" cols="33" />
-      { formik.touched.description && formik.errors.description ? <div className="error">{formik.errors.description}</div> : null}
-
-      <label htmlFor="price" className="label">Price</label>
-      <input name="price" type="number" min="0" step="0.01" className="field" {...formik.getFieldProps('price')} />
-      { formik.touched.price && formik.errors.price ? <div className="error">{formik.errors.price}</div> : null}
-
-      <input type="submit" className="button" value="Submit" />
-    </form>
+const Input = ({label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name } className="label">{label}</label>
+      <input className="field" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
   )
-}
+};
+
+const TextArea = ({label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name } className="label">{label}</label>
+      <textarea className="field" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  )
+};
+
+const ProductForm = ({ product, handleRequest }) => {
+  return (
+    <Formik
+      initialValues={{
+        name: product.name,
+        picture: product.picture,
+        description: product.description,
+        price: product.price
+      }}
+      validate={validate}
+      onSubmit={(values, {setSubmitting}) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          handleRequest();
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      <Form className="form">
+        <Input
+          label="Name"
+          name="name"
+          type="text"
+          placeholder="Enter product name"
+        />
+
+        <Input
+          label="Image"
+          name="picture"
+          type="text"
+          placeholder="Insert image URL"
+        />
+
+        <TextArea
+          label="Description"
+          name="description"
+          rows="5"
+          cols="33"
+        />
+
+        <Input
+          label="Price"
+          name="price"
+          type="number"
+          min="0"
+          step="0.01"
+        />
+
+        <input type="submit" className="button" value="Submit" />
+      </Form>
+    </Formik>
+  );
+};
 
 ProductForm.propTypes = {
   product: PropTypes.object,
@@ -77,45 +119,3 @@ ProductForm.defaultProps = {
 }
 
 export default ProductForm;
-
-      {/*  <Formik*/}
-      {/*    initialValues={{ name: '', picture: '', description: '', price: 0}}*/}
-      {/*    validate={ values => {*/}
-      {/*      const errors = {};*/}
-      {/*      if (!values.name){*/}
-      {/*        errors.name = 'Required';*/}
-      {/*      }*/}
-      {/*      if (!values.picture){*/}
-      {/*        errors.picture = 'Required';*/}
-      {/*      }*/}
-      {/*      if (!values.description){*/}
-      {/*        errors.description = 'Required';*/}
-      {/*      }*/}
-      {/*      if (!values.price){*/}
-      {/*        errors.price = 'Required';*/}
-      {/*      }*/}
-      {/*      return errors;*/}
-      {/*    }}*/}
-      {/*    onSubmit={(values, { setSubmitting }) => {*/}
-      {/*      setTimeout(() => {*/}
-      {/*        alert(JSON.stringify(values, null, 2));*/}
-      {/*        setSubmitting(false);*/}
-      {/*      }, 400);*/}
-      {/*    }}*/}
-      {/*  >*/}
-      {/*    {*/}
-      {/*      ({ isSubmitting }) => (*/}
-      {/*        <Form>*/}
-      {/*          <Field type="text" name="name"/>*/}
-      {/*          <ErrorMessage name="name" component="div" />*/}
-      {/*          <Field type="text" name="picture"/>*/}
-      {/*          <ErrorMessage name="picture" component="div" />*/}
-      {/*          <Field as="textarea" name="description"/>*/}
-      {/*          <ErrorMessage name="description" component="div" />*/}
-      {/*          <Field type="number" name="price" min="0" step="0.01" />*/}
-      {/*          <ErrorMessage name="price" component="div" />*/}
-      {/*        </Form>*/}
-      {/*      )*/}
-      {/*    }*/}
-      {/*  </Formik>*/}
-      {/*</div>*/}
