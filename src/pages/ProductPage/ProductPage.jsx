7959af306b7962cb1f7fb3d757dee5cfc74ba5f6
Oleksandr Berezhnyi <berrezhnoy@gmail.com>
2020-07-09 19:13:15 +0300

    Update styling for product page

--- src/pages/ProductPage/ProductPage.jsx
+++ src/pages/ProductPage/ProductPage.jsx
@@ -8,9 +8,12 @@ import Typography from '@material-ui/core/Typography';
 import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
+import Container from '@material-ui/core/Container';
 
 import { deleteProduct } from '../../utils/fetch';
 import { useProduct } from '../../utils/hooks';
+
+import './style.scss';
 
 const ProductPage = ({ match, history}) => {
   const { id } = match.params;
@@ -30,7 +33,7 @@ const ProductPage = ({ match, history}) => {
   }, [deleteProduct]);
 
   return (
-    <div>
+    <Container maxWidth="sm" className="container">
       <Card>
         <CardActionArea>
           <CardMedia
@@ -39,15 +42,25 @@ const ProductPage = ({ match, history}) => {
             title={product?.name}
           />
           <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {product?.name} - price: ${product?.price}
-            </Typography>
+            <div className="title-box">
+              <Typography gutterBottom variant="h5" component="h2">
+                {product?.name}
+              </Typography>
+              <Typography gutterBottom variant="h6" component="h4">
+                ${product?.price}
+              </Typography>
+            </div>
             <Typography variant="body2" color="textSecondary" component="p">
               {product?.description}
             </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions>
+          <Link to="/products" className="link">
+            <Button size="small" color="primary">
+              {'< Back'}
+            </Button>
+          </Link>
           <Link to={`/products/${id}/edit`} className="link">
             <Button size="small" color="primary">
               Edit
@@ -58,7 +71,7 @@ const ProductPage = ({ match, history}) => {
           </Button>
         </CardActions>
       </Card>
-    </div>
+    </Container>
   )
 }
 
